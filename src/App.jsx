2c5dfcce9bf2ff573ea6409dba65afe074e88f65
silvak <silvak.jeg@gmail.com>
2024-06-04 20:03:44 -0400

    change app

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ function App() {
 
   return (
     <div className="bg-[#2c2c2c] w-full h-[100vh] flex flex-col align items-center">
-        <h1 className="flex text-2xl text-white pt-[20%]">Carousels Advertaising</h1>
-        <div className="flex flex-col gap-2 bg-gray-700 min-w-[240px]  px-8 py-4 rounded-md shadow-lg mt-5">
+        <h1 className="flex text-2xl text-gray-100 font-semibold uppercase pt-[120px]">Carousels Advertaising</h1>
+        <div className="flex flex-col gap-2 bg-gray-700 w-[260px]  px-8 py-4 rounded-md shadow-lg mt-5">
           <Link to={"/cnn"} className="text-gray-300 hover:text-white">Cnn</Link>
           <Link to={"/pais"} className="text-gray-300 hover:text-white">El país</Link>
           <Link to={"/reuters"} className="text-gray-300 hover:text-white">Reuters</Link>
+        </div>
+
+        <div className="flex flex-col gap-2 bg-gray-700 w-[260px]  px-8 py-4 rounded-md shadow-lg mt-5">
+          <p className="text-gray-300">
+          To edit the content of the carousels go to: </p>
+          <a href="https://hygraph.com/" className="text-white hover:text-red-400">Hygraph</a>
         </div>
     </div>
   );
