e2439eb2745dc6e7b5c78e02fe6ce3d73d4b8c5e
Benjamín <benser22@hotmail.com>
2024-09-12 14:34:28 -0300

    feat(persist-state): implement localStorage for mode, project, and campaign selection

--- src/App.jsx
+++ src/App.jsx
@@ -121,14 +121,30 @@ function App() {
       <div className="flex flex-col gap-2 bg-gray-700 w-[260px] px-8 py-4 rounded-md shadow-lg mt-5">
         <button
           onClick={goToAnalytics}
-          className={`text-gray-300 hover:text-white ${
+          className={`text-gray-300 font-semibold hover:text-white ${
             selectedProject && selectedCampaign
               ? 'cursor-pointer'
               : 'cursor-not-allowed'
           }`}
           disabled={!selectedProject || !selectedCampaign}
         >
-          Zafir Analytics
+          <span className="flex w-full items-center justify-center gap-2">
+            Go to Page
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </span>
         </button>
 
         <label className="text-gray-300">Select Project:</label>
