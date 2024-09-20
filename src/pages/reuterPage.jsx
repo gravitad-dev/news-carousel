014b1b7386d6b88e4476360c9f3d2aa79772b42d
Benjamín Serrano <benser22@hotmail.com>
2024-09-20 15:03:06 -0300

    try deploy

--- src/pages/reuterPage.jsx
+++ src/pages/reuterPage.jsx
@@ -1,5 +1,35 @@
-import Frame from '../components/Frame';
+// ReuterPage.jsx
+import React, { useEffect, useState } from 'react';
 
-export function ReuterPage() {
-  return <Frame src="https://reuters2.vercel.app/" />;
-}
+export const ReuterPage = () => {
+  const [iframeSrc, setIframeSrc] = useState('');
+
+  useEffect(() => {
+    // Llama a la ruta del backend para obtener el HTML
+    fetch('https://za-backend-1.onrender.com/scraping') // Cambia esto si es necesario
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then((html) => {
+        // Crea un objeto Blob y un URL para el iframe
+        const blob = new Blob([html], { type: 'text/html' });
+        setIframeSrc(URL.createObjectURL(blob));
+      })
+      .catch((error) => {
+        console.error('Error fetching HTML:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        src={iframeSrc}
+        style={{ width: '100%', height: '500px', border: 'none' }}
+        title="Reuter Iframe"
+      />
+    </div>
+  );
+};
